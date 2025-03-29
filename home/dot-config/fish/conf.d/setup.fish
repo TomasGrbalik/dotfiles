22128ac88206e8b130fe648e33e656f42955f9ea
Tomas Grbalik <grbalik@Ilae.local>
2025-03-29 11:28:43 +0100

    Switch from starship to tide

--- home/dot-config/fish/conf.d/setup.fish
+++ home/dot-config/fish/conf.d/setup.fish
@@ -12,5 +12,6 @@ end
 
 set -x SSH_AUTH_SOCK /Users/$USER/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
 
-starship init fish | source
-enable_transience
+# starship init fish | source
+tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Many icons' --transient=No
+# enable_transience
